f2588791ce6944426728243444ccf4f6cf47104a
Dan Rahmel <drahmel7@gmail.com>
2024-04-26 16:43:12 +0000

    DJR:Added start testnet miner

---
+++ testnet_start_miner.sh
@@ -0,0 +1,1 @@
+python3 -m neurons.miner --netuid 138 --blacklist.force_validator_permit --subtensor.network test --wallet.name ap-test-miner-coldkey --wallet.hotkey ap-test-miner-hotkey --logging.debug
---
+++ testnet_start_validator.sh
@@ -0,0 +1,1 @@
+python3 -m neurons.validator --netuid 138 --subtensor.network test --wallet.name ap-testnet-coldkey --wallet.hotkey ap-testnet-hotkey --logging.debug
