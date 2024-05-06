6d6b0c6a9ac6ed9eed1cf8cc8e89958cceba84e2
Dan Rahmel <drahmel7@gmail.com>
2024-05-06 16:47:50 +0000

    DJR:Added gitignore

---
+++ web/example_put.sh
@@ -0,0 +1,2 @@
+curl -XPUT http://localhost:8000/api/v1/conversation/record/10  -H "Content-Type: application/json" -d '{"hotkey": "5FqDFb7kBwYrjm91CV6bPzbRKu3Q2v8P8ZkWrLhM5N3f8RSh", "tags": ["12345"]}'
+
