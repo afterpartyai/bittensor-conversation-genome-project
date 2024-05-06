e11ec644fedc10c602370f1f167dff6c633e482c
Dan Rahmel <drahmel7@gmail.com>
2024-05-06 18:21:26 +0000

    DJR:Converted validator prints to logs

--- tests/test_validator_lib.py
+++ tests/test_validator_lib.py
@@ -33,7 +33,7 @@ async def test_full():
         wl.init_wandb()
     # Config variables
     c.set('system', 'mode', 'test')
-    miner_uids = [1,2,3,4,5,6,7,8,9]
+    miner_uids = []
 
     vl = ValidatorLib()
     el = Evaluator()
