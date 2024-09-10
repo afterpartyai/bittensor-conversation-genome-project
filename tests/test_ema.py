ef31c621d8ecbd57b75df15b540e4a6c7614154a
Dan Rahmel <drahmel7@gmail.com>
2024-09-10 23:53:54 +0000

    DJR:Added skeltons of 2 tests

--- tests/test_ema.py
+++ tests/test_ema.py
@@ -46,8 +46,39 @@ class TemplateEmaTestCase(unittest.TestCase):
        assert ema_scores[1] == 0.2
        assert ema_scores[2] == 0.0
 
+    def test_great_score_variation(self):
+       uids = [1,2,3]
+       ft = torch.FloatTensor([0.1, 0.5, 1.0])
+       print(f"Testing: ", ft, uids)
+       ema_scores = self.update_scores(ft, uids)
+       assert torch.isnan(ema_scores).any() == False
+       assert ema_scores[0] == 1.0
+       assert ema_scores[1] == 0.2
+       assert ema_scores[2] == 0.0
+
+    def test_small_variation(self):
+       uids = [1,2,3]
+       ft = torch.FloatTensor([0.47, 0.5, 0.52])
+       print(f"Testing: ", ft, uids)
+       ema_scores = self.update_scores(ft, uids)
+       assert torch.isnan(ema_scores).any() == False
+       assert ema_scores[0] == 0.48
+       assert ema_scores[1] == 0.5
+       assert ema_scores[2] == 0.51
+
+    def test_no_variation(self):
+       uids = [1,2,3]
+       ft = torch.FloatTensor([0.5, 0.5, 0.5])
+       print(f"Testing: ", ft, uids)
+       ema_scores = self.update_scores(ft, uids)
+       assert torch.isnan(ema_scores).any() == False
+       assert ema_scores[0] == 0.5
+       assert ema_scores[1] == 0.5
+       assert ema_scores[2] == 0.5
 
 
     def update_scores(self, rewards: torch.FloatTensor, uids: List[int]):
         #return torch.FloatTensor([0.4,0.5,0.6])
-        return torch.nan_to_num(rewards, 0.0)
+        rewards = torch.nan_to_num(rewards, 0.0)
+        rewards = torch.clamp(rewards, min=0.0, max=1.0)
+        return rewards
