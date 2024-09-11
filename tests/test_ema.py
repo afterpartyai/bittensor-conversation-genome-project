f8d655eaf6459acfa0fc1d57372c7b40a4003f92
root <john@afterparty.com>
2024-09-11 16:33:41 +0000

    finalized nan testing and improved small variantion case

--- tests/test_ema.py
+++ tests/test_ema.py
@@ -28,27 +28,30 @@ class TemplateEmaTestCase(unittest.TestCase):
         pass
 
     def test_nan(self):
+       thisdevice = "cuda" if torch.cuda.is_available() else "cpu"
        uids = [1,2,3]
-       ft = torch.FloatTensor([0.1, float('nan') ,0.3])
-       assert torch.isnan(ft).any() == True
-       print(f"Testing: ", ft, uids)
-       ema_scores = self.update_scores(ft, uids)
+       rewards = torch.tensor([0.1, float('nan') ,0.3],device=thisdevice)
+       scores = torch.tensor([0.004455, 0.035550, 0.120000, 0.284445, 0.555550],device=thisdevice)
+       ema_scores = torch.tensor([0.1,0.2,0.3,0.4,0.5],device=thisdevice)
+       moving_average_alpha = 0.1
+       device = "cuda"
+       neurons = 5
+       nonlinear_power = 3
+       #print(f"Testing: ", rewards, uids)
+       scores, ema_scores = self.vl.update_scores(rewards, uids, ema_scores, scores, moving_average_alpha, device, neurons, nonlinear_power)
+       print(f"Testing nan: ", scores, ema_scores)
        assert torch.isnan(ema_scores).any() == False
-       assert ema_scores[0] == 0.1
-       assert ema_scores[1] == 0.0
-       assert ema_scores[2] == 0.3
-
-    def test_out_of_range(self):
-       uids = [1,2,3]
-       ft = torch.FloatTensor([10.1, 0.2 ,-0.3])
-       print(f"Testing: ", ft, uids)
-       ema_scores = self.update_scores(ft, uids)
-       assert torch.isnan(ema_scores).any() == False
-       assert ema_scores[0] == 1.0
-       assert ema_scores[1] == 0.2
-       assert ema_scores[2] == 0.0
-
-    #rewards, uids, ema_scores, scores, moving_average_alpha, device, neurons, nonlinear_power
+       assert torch.isnan(scores).any() == False
+       assert ema_scores[0].item() == pytest.approx(0.1,abs=1e-3)
+       assert ema_scores[1].item() == pytest.approx(0.1900,abs=1e-3)
+       assert ema_scores[2].item() == pytest.approx(0.2700,abs=1e-3)       
+       assert ema_scores[3].item() == pytest.approx(0.3900,abs=1e-3)
+       assert ema_scores[4].item() == pytest.approx(0.5,abs=1e-3)
+       assert scores[0].item() == pytest.approx(0.0047,abs=1e-3)
+       assert scores[1].item() == pytest.approx(0.0324,abs=1e-3)
+       assert scores[2].item() == pytest.approx(0.0929,abs=1e-3)
+       assert scores[3].item() == pytest.approx(0.2800,abs=1e-3)
+       assert scores[4].item() == pytest.approx(0.5900,abs=1e-3)
 
     def test_great_score_variation(self):
        thisdevice = "cuda" if torch.cuda.is_available() else "cpu"
@@ -79,9 +82,9 @@ class TemplateEmaTestCase(unittest.TestCase):
     def test_small_variation(self):
        thisdevice = "cuda" if torch.cuda.is_available() else "cpu"
        uids = [1,2,3]
-       rewards = torch.tensor([0.49, 0.5, 0.51],device=thisdevice)
-       scores = torch.tensor([0.004455, 0.035550, 0.120000, 0.284445, 0.555550],device=thisdevice)
-       ema_scores = torch.tensor([0.1,0.2,0.3,0.4,0.5],device=thisdevice)
+       rewards = torch.tensor([0.285, 0.295, 0.32],device=thisdevice)
+       scores = torch.tensor([0.174646, 0.183967, 0.193342, 0.213330, 0.234716],device=thisdevice)
+       ema_scores = torch.tensor([0.29,0.295,0.3,0.31,0.32],device=thisdevice)
        moving_average_alpha = 0.1
        device = "cuda"
        neurons = 5
@@ -90,16 +93,16 @@ class TemplateEmaTestCase(unittest.TestCase):
        print(f"Testing small variation: ", scores, ema_scores)
        assert torch.isnan(ema_scores).any() == False
        assert torch.isnan(scores).any() == False
-       assert ema_scores[0].item() == pytest.approx(0.1, abs=1e-3)
-       assert ema_scores[1].item() == pytest.approx(0.2290, abs=1e-3)
-       assert ema_scores[2].item() == pytest.approx(0.3200, abs=1e-3)
-       assert ema_scores[3].item() == pytest.approx(0.4110, abs=1e-3)
-       assert ema_scores[4].item() == pytest.approx(0.5, abs=1e-3)
-       assert scores[0].item() == pytest.approx(0.0042, abs=1e-3)
-       assert scores[1].item() == pytest.approx(0.0500, abs=1e-3)
-       assert scores[2].item() == pytest.approx(0.1364, abs=1e-3)
-       assert scores[3].item() == pytest.approx(0.2890, abs=1e-3)
-       assert scores[4].item() == pytest.approx(0.5204, abs=1e-3)
+       assert ema_scores[0].item() == pytest.approx(0.2900, abs=1e-3)
+       assert ema_scores[1].item() == pytest.approx(0.2940, abs=1e-3)
+       assert ema_scores[2].item() == pytest.approx(0.2995, abs=1e-3)
+       assert ema_scores[3].item() == pytest.approx(0.3110, abs=1e-3)
+       assert ema_scores[4].item() == pytest.approx(0.3200, abs=1e-3)
+       assert scores[0].item() == pytest.approx(0.1748, abs=1e-3)
+       assert scores[1].item() == pytest.approx(0.1821, abs=1e-3)
+       assert scores[2].item() == pytest.approx(0.1926, abs=1e-3)
+       assert scores[3].item() == pytest.approx(0.2156, abs=1e-3)
+       assert scores[4].item() == pytest.approx(0.2349, abs=1e-3)
 
     def test_no_variation(self):
        thisdevice = "cuda" if torch.cuda.is_available() else "cpu"
