459bfe53f261ed8349170dddd19d798137ece912
root <john@afterparty.com>
2024-09-23 16:03:13 -0400

    removing UID file changes that were pushed accidentally

--- conversationgenome/utils/uids.py
+++ conversationgenome/utils/uids.py
@@ -71,5 +71,4 @@ def get_random_uids(
             k - len(candidate_uids),
         )
     uids = torch.tensor(random.sample(available_uids, k))
-    uids = torch.tensor([19,29,33])
     return uids
