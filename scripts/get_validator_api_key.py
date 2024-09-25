262c4ca0f18d58fcc5a14df6c371097831cdebf1
Dan Rahmel <drahmel7@gmail.com>
2024-09-25 16:50:29 +0000

    DJR:Refactored account gen into class

--- scripts/get_validator_api_key.py
+++ scripts/get_validator_api_key.py
@@ -1,9 +1,3 @@
-import bittensor as bt
-import base58
-from hashlib import blake2b
-from scalecodec.utils.ss58 import ss58_decode
-import requests
-
 CYAN = "\033[96m" # field color
 GREEN = "\033[92m" # indicating success
 RED = "\033[91m" # indicating error
@@ -11,26 +5,40 @@ YELLOW = '\033[0;33m'
 COLOR_END = '\033[m'
 DIVIDER = '_' * 120
 
+import bittensor as bt
+import base58
+from hashlib import blake2b
+
+ss58_decode = None
+try:
+    from scalecodec.utils.ss58 import ss58_decode
+except:
+    print("{RED}scalecodec is not installed. Try: pip install xxx")
+import requests
+
+
 
 class CgpApiLib():
     def get(self, ss58_hotkey, netuid = 1, verbose=False):
         subnet = bt.metagraph(netuid)
         if not ss58_hotkey in subnet.hotkeys:
-            print(f"Hotkey {ss58_hotkey} not registered on subnet. Aborting.")
+            print(f"{RED}Hotkey {ss58_hotkey} not registered on subnet. Aborting.{COLOR_END}")
             if verbose:
                 print("SUBNET HOTKEYS", subnet.hotkeys)
             return
         my_uid = subnet.hotkeys.index( ss58_hotkey )
         print(f"UID for Hotkey: {ss58_hotkey} : {my_uid}")
         if ss58_hotkey not in subnet.hotkeys:
-            print(f"Hotkey {ss58_hotkey} is not registered in subnet list ({len(subnet.hotkeys)})")
+            print(f"{RED}Hotkey {ss58_hotkey} is not registered in subnet list ({len(subnet.hotkeys)}). Aborting.{COLOR_END}")
+            return
         else:
-            print(f"Hotkey {ss58_hotkey} ")
-            print(f"HOTKEY is registered: {subnet.hotkeys[my_uid]}, COLDKEY:{subnet.coldkeys[my_uid]}, VALIDATOR:{subnet.validator_permit[my_uid]}, STAKE:{subnet.stake[my_uid]}")
-            coldkey = subnet.coldkeys[my_uid]
+            validator_info = {"subnet_id": netuid, "uid":my_uid, "hotkey": ss58_hotkey, "coldkey":subnet.coldkeys[my_uid], "is_validator": subnet.validator_permit[my_uid], "total_stake":subnet.stake[my_uid]}
+            print(f"{GREEN}HOTKEY {ss58_hotkey} is registered on subnet{COLOR_END}: COLDKEY:{validator_info['coldkey']}, IS VALIDATOR:{validator_info['is_validator']}, TOTAL STAKE:{validator_info['total_stake']}")
             if verbose:
                 # Display properties for this uid
                 self.list_wallets_properties(subnet, uid=my_uid, tensor_len=len(subnet.hotkeys))
+            return validator_info
+
 
     def list_wallets_properties(self, obj, uid=5, tensor_len=1024):
         properties = dir(obj)
@@ -42,6 +50,12 @@ class CgpApiLib():
             except Exception as e:
                 pass
                 #print(f"{prop}: {e}")
+
+    def get_account_from_coldkey(self, ss58_coldkey):
+        if not ss58_decode:
+            print("{RED}scalecodec is not installed. Aborting.")
+            return
+        return ss58_decode(ss58_coldkey, valid_ss58_format=42)
 
 
 if __name__ == "__main__":
@@ -61,9 +75,7 @@ if __name__ == "__main__":
             ss58_hotkey = '5G1awceKsZ4MKTCSkT7qqzhQ5Z3WjWfE5cifCm237Vz3fmN3' # Random validator hotkey
         print(f"{YELLOW}Checking subnet {subnet_id} for hotkey {ss58_hotkey}...{COLOR_END}")
         print(f'{YELLOW}{DIVIDER}{COLOR_END}')
-        cal.get(ss58_hotkey, subnet_id)
-        # Works
-        #ss58_hotkey = "5GnBLhJG16Ra2WMdKGPUpLopu5wsFPTY6pGyf9u3N1T4cqsC"
-        account_id = ss58_decode(ss58_hotkey, valid_ss58_format=42)
+        validator_info = cal.get(ss58_hotkey, subnet_id)
+        account_id = cal.get_account_from_coldkey(ss58_hotkey)
         print(f"The decoded account ID for the address {ss58_hotkey} is: {account_id}")
 
