f15b53db9cfb4aebfe5beee77f88f7d2e76d862a
Dan Rahmel <drahmel7@gmail.com>
2024-09-25 20:18:15 +0000

    DJR:Added example validators reporting for testing

--- scripts/get_validator_api_key.py
+++ scripts/get_validator_api_key.py
@@ -30,7 +30,7 @@ class ReadyAiApiLib():
     api_root_url = "https://conversations.xyz"
     api_message_route = "/api/v1/generate_message"
     api_key_route = "/api/v1/generate_api_key"
-    minimum_stake = 0.5
+    minimum_stake = 1.0
 
     def __init__(self, test_mode=False):
         self.test_mode = test_mode
@@ -42,10 +42,26 @@ class ReadyAiApiLib():
         if not ss58_coldkey in subnet.coldkeys:
             print(f"{RED}Coldkey {ss58_coldkey} not registered on subnet. Aborting.{COLOR_END}")
             if verbose:
-                print("SUBNET COLDKEYS", subnet.coldkeys)
+                #print("SUBNET COLDKEYS", subnet.coldkeys)
+                found_validator_staked = False
+                found_validator_unstaked = False
+                found_non_validator = False
+                for idx, coldkey in enumerate(subnet.coldkeys):
+                    if not found_validator_staked and subnet.validator_permit[idx] and subnet.stake[idx] >= self.minimum_stake:
+                        print(f"Validator staked: {coldkey} stake: {subnet.stake[idx]}")
+                        found_validator_staked = True
+                    elif not found_validator_unstaked and subnet.validator_permit[idx] and subnet.stake[idx] < self.minimum_stake:
+                        print(f"Validator unstaked: {coldkey} stake: {subnet.stake[idx]}")
+                        found_validator_unstaked = True
+                    elif not found_non_validator and not subnet.validator_permit[idx]:
+                        print(f"Not Validator: {coldkey} stake: {subnet.stake[idx]}")
+                        found_non_validator = True
             return
         my_uid = subnet.coldkeys.index( ss58_coldkey )
         print(f"Subnet UID for coldkey: {ss58_coldkey} : {my_uid}")
+        if verbose:
+            # Display properties for this uid
+            self.list_wallets_properties(subnet, uid=my_uid, tensor_len=len(subnet.coldkeys))
         if not ss58_coldkey in subnet.coldkeys:
             print(f"{RED}Coldkey {ss58_coldkey} is not registered in subnet list ({len(subnet.coldkeys)}). Aborting.{COLOR_END}")
             return
@@ -62,9 +78,6 @@ class ReadyAiApiLib():
 
         validator_info = {"subnet_id": netuid, "uid":my_uid, "coldkey": ss58_coldkey, "hotkey":subnet.hotkeys[my_uid], "is_validator": is_validator, "total_stake":total_stake}
         print(f"{GREEN}COLDKEY {ss58_coldkey} is registered on subnet{COLOR_END}: COLDKEY:{validator_info['coldkey']}, IS VALIDATOR:{validator_info['is_validator']}, TOTAL STAKE:{validator_info['total_stake']}")
-        if verbose:
-            # Display properties for this uid
-            self.list_wallets_properties(subnet, uid=my_uid, tensor_len=len(subnet.hotkeys))
         return validator_info
 
     def list_wallets_properties(self, obj, uid=5, tensor_len=1024):
@@ -205,7 +218,9 @@ if __name__ == "__main__":
             ss58_coldkey = coldkey_obj.ss58_address
         else:
             coldkey_object = None
-            ss58_coldkey = "5DaUQrFC3kEuqCkvBELUs47pJpNoqXTTiHop7c3pcq1KngQo"
+            ss58_coldkey = "5HB4EbnVdtmBVdnrUxce2jDE7BJJM1cv9xV1X1wiriyeKy8R" # Not validator
+            ss58_coldkey = "5GTVbEdiPtmJ1XWG7FgKaYifLciwWUDDXeauVKgpzKkNDCQL" # Unstaked
+            ss58_coldkey = '5GZSAgaVGQqegjhEkxpJjpSVLVmNnE2vx2PFLzr7kBBMKpGQ' # Staked
         print(f"{YELLOW}Checking subnet {subnet_id} for coldkey {ss58_coldkey}...{COLOR_END}")
         print(f'{YELLOW}{DIVIDER}{COLOR_END}')
         validator_info = raal.get_validator_info(ss58_coldkey, subnet_id)
