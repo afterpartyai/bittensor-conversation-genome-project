de48953c8b7c14fa5bafc5a0a22ca83f6db544e7
Dan Rahmel <drahmel7@gmail.com>
2024-10-29 22:19:00 +0000

    DJR:Added basic tiles and SPA route

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,10 +1,23 @@
-function displayTaskTypes() {
+let Utils = {};
+Utils.getRequest = (name, defaultVal) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name) || defaultVal;
+}
+
+let Api = {};
+Api.getTaskTypes = (callback) => {
+    $.get("/", (o) => {
+       console.log("O", o);
+    });
+}
+
+function renderHome() {
     console.log("Helo");
     const taskTypes = [
-        { title:"A", image_url:'/static/images/icons/ad_analysis.jpg' },
-        { title:"B", image_url:'/static/images/icons/public_data_analysis.jpg' },
-        { title:"C", image_url:'/static/images/icons/social_media_analysis.jpg' },
-        { title:"D", image_url:'/static/images/icons/survey_analysis.jpg' },
+        { title:"Adword Analysis/Context generation", image_url:'/static/images/icons/ad_analysis.jpg' },
+        { title:"Public Data Analysis", image_url:'/static/images/icons/public_data_analysis.jpg' },
+        { title:"Social Media Analysis", image_url:'/static/images/icons/social_media_analysis.jpg' },
+        { title:"Survey Data Analysis", image_url:'/static/images/icons/survey_analysis.jpg' },
     ]
     const sel = $("main .tileContainer");
     for(let idx in taskTypes) {
@@ -23,7 +36,11 @@ function displayTaskTypes() {
     }
 }
 
+
 $(document).ready(function() {
-    displayTaskTypes();
+    route = Utils.getRequest('route', 'home');
+    if(route == 'home') {
+        renderHome();
+    }
 });
 
