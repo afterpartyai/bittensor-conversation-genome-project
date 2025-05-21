dc6df78659e410780056c439088537b48049826d
duotang12 <j.bouchard@hotmail.fr>
2025-05-20 21:26:23 -0400

    only count str specifically

--- conversationgenome/analytics/WandbLib.py
+++ conversationgenome/analytics/WandbLib.py
@@ -135,8 +135,8 @@ class WandbLib:
 
             for value in data.values():
                 try:
-                    value_to_count = value if isinstance(value, str) else str(value)
-                    estimated_lines += value_to_count.count('\n') + 1
+                    if isinstance(value, str):
+                        estimated_lines += value.count('\n') + 1
                 except Exception as e:
                     bt.logging.debug(f"Failed to count log lines for value: {value} — {e}")
                     estimated_lines += 1
