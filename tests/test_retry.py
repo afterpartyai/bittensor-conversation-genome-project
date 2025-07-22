87bf920f528912484d60c952465988daffb41dcb
duotang12 <j.bouchard@hotmail.fr>
2025-07-22 18:25:41 -0400

    Add test for other http errors

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -200,7 +200,7 @@ async def test_when_receiving_408_responses_then_retry_only_those_requests(
     # --------------------------
     # Validate Retry Behavior
     # --------------------------
-    # Only the second call has retries
+    # The second call has 1 axon because it retries only the 408 response
     all_calls = validator.dendrite.forward.await_args_list
     for idx, call in enumerate(all_calls):
         _, kwargs = call
@@ -218,7 +218,7 @@ async def test_when_receiving_408_responses_then_retry_only_those_requests(
     # Final Assertions
     # --------------------------
     # Forward has been called 11 times (10 initial + 1 retry)
-    # Evaluate has been called 10 times once for 10 loops
+    # Evaluate has been called 10 times, once for 10 loops
     assert result is True
     assert validator.dendrite.forward.call_count == 11
     assert mock_evaluator.return_value.evaluate.await_count == 10
@@ -230,7 +230,7 @@ async def test_when_receiving_408_responses_then_retry_only_those_requests(
 @patch("bittensor.wallet")
 @patch("bittensor.subtensor")
 @patch("bittensor.metagraph")
-async def test_when_no_408_then_dont_retry_anything(
+async def test_when_no_408_then_retry_nothing(
     mock_metagraph,
     mock_subtensor,
     mock_wallet,
@@ -264,7 +264,7 @@ async def test_when_no_408_then_dont_retry_anything(
     result = await validator.forward(test_mode=True)
 
     # --------------------------
-    # Validate No Retry
+    # Validate Axons
     # --------------------------
     all_calls = validator.dendrite.forward.await_args_list
     for _, call in enumerate(all_calls):
@@ -278,8 +278,69 @@ async def test_when_no_408_then_dont_retry_anything(
     # --------------------------
     # Final Assertions
     # --------------------------
-    # Forward has been called 10 times (10 initial)
-    # Evaluate has been called 10 times once for 10 loops
+    # Forward has been called 10 times meaning no retries
+    # Evaluate has been called 10 times, once for 10 loops
+    assert result is True
+    assert validator.dendrite.forward.call_count == 10
+    assert mock_evaluator.return_value.evaluate.await_count == 10
+
+@pytest.mark.asyncio
+@patch("neurons.validator.ValidatorLib", autospec=True)
+@patch("conversationgenome.base.validator.ValidatorLib", autospec=True)
+@patch("neurons.validator.Evaluator", autospec=True)
+@patch("bittensor.wallet")
+@patch("bittensor.subtensor")
+@patch("bittensor.metagraph")
+async def test_when_other_errors_than_408_then_retry_nothing(
+    mock_metagraph,
+    mock_subtensor,
+    mock_wallet,
+    mock_evaluator,
+    mock_validator_lib2,
+    mock_validator_lib,
+):
+    vl_mock, el_mock, mock_wallet_instance, mock_subtensor_instance, mock_metagraph_instance, validator = setup()
+    mock_metagraph.return_value = mock_metagraph_instance
+    mock_subtensor.return_value = mock_subtensor_instance
+    mock_wallet.return_value = mock_wallet_instance
+    mock_evaluator.return_value = el_mock
+    mock_validator_lib2.return_value = vl_mock
+    mock_validator_lib.return_value = vl_mock
+
+    # --------------------------
+    # Responses & Side Effect
+    # --------------------------
+    miner1_500_response = DummyResponse("miner1", 500)
+    miner2_404_response = DummyResponse("miner2", 404)
+    miner3_303_response = DummyResponse("miner3", 303)
+
+    def forward_side_effect(*args, **kwargs):
+        return [miner1_500_response, miner2_404_response, miner3_303_response]
+
+    validator.dendrite.forward = AsyncMock(side_effect=forward_side_effect)
+
+    # --------------------------
+    # Execute
+    # --------------------------
+    result = await validator.forward(test_mode=True)
+
+    # --------------------------
+    # Validate Axons
+    # --------------------------
+    all_calls = validator.dendrite.forward.await_args_list
+    for _, call in enumerate(all_calls):
+        _, kwargs = call
+        forwarded_axons = kwargs.get("axons", None)
+
+        assert len(forwarded_axons) == 3
+        for axon in forwarded_axons:
+            assert axon.hotkey in {"miner1", "miner2", "miner3"}
+
+    # --------------------------
+    # Final Assertions
+    # --------------------------
+    # Forward has been called 10 times meaning no retries
+    # Evaluate has been called 10 times, once for 10 loops
     assert result is True
     assert validator.dendrite.forward.call_count == 10
     assert mock_evaluator.return_value.evaluate.await_count == 10
