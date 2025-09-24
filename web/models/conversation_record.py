8d6d5e527dfc630c5110f16d687a20120ff744c2
duotang12 <j.bouchard@hotmail.fr>
2025-09-24 14:52:20 -0400

    remove topic attribute

--- web/models/conversation_record.py
+++ web/models/conversation_record.py
@@ -1,29 +1,29 @@
-from typing import List, Dict, Any
 from datetime import datetime
+from typing import Any, Dict, List
+
 from pydantic import BaseModel
+
+from conversationgenome.utils.types import ForceStr
 
 
 class Participant(BaseModel):
-    idx: int
-    guid: int
+    idx: ForceStr
+    guid: ForceStr
     title: str
 
 
 class ConversationData(BaseModel):
-    id: str
-    guid: int
-    topic: str
+    id: ForceStr
+    guid: ForceStr
     lines: List[List[Any]]
     participants: Dict[str, Participant]
 
 
 class ConversationRecord(BaseModel):
-    id: int
-    source_id: int
-    guid: str
+    id: ForceStr
+    source_id: ForceStr
+    guid: ForceStr
     idx: int
-    topic: str
     data: ConversationData
     created_at: datetime
     updated_at: datetime
-
