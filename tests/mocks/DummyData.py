425bb48df7cab89edfb2c0f561bddfd69cd0cc1a
jobou558 <jobou558@ulaval.ca>
2026-01-09 15:01:03 -0500

    add missing conversation_quality_metadata_high to DummyData

--- tests/mocks/DummyData.py
+++ tests/mocks/DummyData.py
@@ -404,8 +404,10 @@ class DummyData:
         return try_parse_task_bundle(DummyData.webpage_metadata_generation_task_bundle_json())
 
     @staticmethod
-    def setup_webpage_metadata_generation_task_bundle() -> TaskBundle:
-        task_bundle = try_parse_task_bundle(DummyData.webpage_metadata_generation_task_bundle_json())
-        task_bundle.input.data.indexed_windows = DummyData.windows()
-        task_bundle.input.metadata = DummyData.metadata()
-        return task_bundle
+    def conversation_quality_metadata_high():
+        from conversationgenome.api.models.conversation_metadata import ConversationQualityMetadata
+        return ConversationQualityMetadata(
+            quality_score=9,
+            primary_reason="High quality conversation",
+            reason_details="Detailed analysis shows high engagement"
+        )
